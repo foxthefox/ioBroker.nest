65cc095f54b3fbc20103d5ad9067800f1bf25202
foxthefox <github@wysiwis.net>
2018-02-18 01:46:14 +0100

    Create words.js

---
+++ admin/words.js
@@ -0,0 +1,9 @@
+    // Dictionary (systemDictionary is global variable from adapter-settings.js)
+    systemDictionary = {
+        "nest adapter settings": {"de": "nest adapter setting", "en": "nest adapter setting", "ru": "nest adapter setting"},
+        "on save adapter restarts with new config immediately": {
+            "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
+            "en": "on save adapter restarts with new config immediately",
+            "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями"
+        }
+    };
