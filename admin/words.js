3ddb65cf84921ce921bf43facbb71a4f45435c40
foxthefox <github@wysiwis.net>
2018-02-20 21:42:57 +0100

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -1,9 +1,58 @@
-    // Dictionary (systemDictionary is global variable from adapter-settings.js)
-    systemDictionary = {
-        "nest adapter settings": {"de": "nest adapter setting", "en": "nest adapter setting", "ru": "nest adapter setting"},
-        "on save adapter restarts with new config immediately": {
-            "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
-            "en": "on save adapter restarts with new config immediately",
-            "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями"
-        }
-    };
+// Dictionary
+systemDictionary = {
+  "create developer login and create new product (any)": {
+    "en": "create developer login and create new product (any)",
+    "de": "Entwickleranmeldung erstellen und neues Produkt erstellen (beliebig)",
+    "ru": "создайте логин разработчика и создайте новый продукт (любой)",
+    "pt": "Crie o login do desenvolvedor e crie um novo produto (qualquer)",
+    "nl": "maak ontwikkelaar login en maak een nieuw product (elke)",
+    "fr": "créer une connexion développeur et créer un nouveau produit (n'importe quel)",
+    "it": "crea il login dello sviluppatore e crea un nuovo prodotto (qualsiasi)",
+    "es": "crear inicio de sesión de desarrollador y crear nuevo producto (cualquiera)",
+    "pl": "utwórz login programisty i utwórz nowy produkt (dowolny)"
+  },
+  "use product_id, product_secret, pin to get access_token": {
+    "en": "use product_id, product_secret, pin to get access_token",
+    "de": "Verwenden Sie product_id, product_secret, pin, um access_token zu erhalten",
+    "ru": "используйте product_id, product_secret, pin, чтобы получить access_token",
+    "pt": "use product_id, product_secret, pin para obter access_token",
+    "nl": "gebruik product_id, product_secret, pin om toegangstoken te krijgen",
+    "fr": "utilisez product_id, product_secret, pin pour obtenir access_token",
+    "it": "usa product_id, product_secret, pin per ottenere access_token",
+    "es": "use product_id, product_secret, pin para obtener access_token",
+    "pl": "użyj product_id, product_secret, pin, aby uzyskać access_token"
+  },
+  "Try to push the Search Button to find Nest devices in your home.": {
+    "en": "Try to push the Search Button to find Nest devices in your home.",
+    "de": "Versuche, die Suchtaste zu drücken, um Nest-Geräte in deinem Zuhause zu finden.",
+    "ru": "Попробуйте нажать кнопку поиска, чтобы найти устройства Nest в вашем доме.",
+    "pt": "Tente empurrar o botão Pesquisar para encontrar os dispositivos Nest na sua casa.",
+    "nl": "Probeer op de zoekknop te drukken om Nest-apparaten thuis te vinden.",
+    "fr": "Essayez d'appuyer sur le bouton de recherche pour trouver les appareils Nest dans votre maison.",
+    "it": "Prova a premere il pulsante Cerca per trovare i dispositivi Nest nella tua casa.",
+    "es": "Intenta presionar el botón de búsqueda para encontrar dispositivos Nest en tu hogar.",
+    "pl": "Spróbuj nacisnąć przycisk wyszukiwania, aby znaleźć urządzenia Nest w domu."
+  },
+  "If you see blue delete buttons, save settings first and open again the page.": {
+    "en": "If you see blue delete buttons, save settings first and open again the page.",
+    "de": "Wenn Sie blaue Löschschaltflächen sehen, speichern Sie zunächst die Einstellungen und öffnen Sie die Seite erneut.",
+    "ru": "Если вы видите синие кнопки удаления, сначала сохраните настройки и снова откройте страницу.",
+    "pt": "Se você ver os botões de exclusão azuis, salve as configurações primeiro e abra novamente a página.",
+    "nl": "Als u blauwe wisknoppen ziet, slaat u eerst de instellingen op en opent u de pagina opnieuw.",
+    "fr": "Si vous voyez des boutons de suppression bleus, enregistrez d'abord les paramètres et ouvrez à nouveau la page.",
+    "it": "Se vedi i pulsanti di cancellazione blu, salva prima le impostazioni e riapri la pagina.",
+    "es": "Si ve botones azules para borrar, primero guarde la configuración y abra de nuevo la página.",
+    "pl": "Jeśli zobaczysz niebieskie przyciski usuwania, najpierw zapisz ustawienia i otwórz ponownie stronę."
+  },
+  "delete": {
+    "en": "delete",
+    "de": "löschen",
+    "ru": "Удалить",
+    "pt": "excluir",
+    "nl": "verwijderen",
+    "fr": "effacer",
+    "it": "Elimina",
+    "es": "borrar",
+    "pl": "kasować"
+  }
+};
